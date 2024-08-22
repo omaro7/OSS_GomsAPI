e6762ceab34cc6218aa89eaa00023d81d99d0074
JUNGHOON HAN <omaro7@hanmail.net>
2024-08-22 17:43:08 +0900

    add @DateTimeFormat

--- src/main/java/kr/co/goms/web/oss/biz/service/dto/FieldCategoryUpdInDto.java
+++ src/main/java/kr/co/goms/web/oss/biz/service/dto/FieldCategoryUpdInDto.java
@@ -1,17 +1,17 @@
 package kr.co.goms.web.oss.biz.service.dto;
 
-import java.util.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
-@Schema(description = "Field_category Field_categoryUpdInDto")
+@Schema(description = "FieldCategory FieldCategoryUpdInDto")
 public class FieldCategoryUpdInDto {
     private Long fieldCategoryIdx;
     private String fieldCategoryName;
     private Boolean useflag;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")	
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private String regdate;
 }
